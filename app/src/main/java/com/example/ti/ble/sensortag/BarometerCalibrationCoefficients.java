82a2329290ace73143537582d2da609d4fb012a5
dontworryaboutit <searing.michael@gmail.com>
2014-10-30 19:15:01 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/ti/ble/sensortag/BarometerCalibrationCoefficients.java
@@ -0,0 +1,12 @@
+package com.example.ti.ble.sensortag;
+
+import java.util.List;
+
+/**
+ * As a last-second hack i'm storing the barometer coefficients in a global.
+ */
+public enum BarometerCalibrationCoefficients {
+  INSTANCE;
+  volatile public List<Integer> barometerCalibrationCoefficients;
+  volatile public double heightCalibration;
+}
---
+++ app/src/main/java/com/example/ti/ble/sensortag/LicenseDialog.java
@@ -0,0 +1,43 @@
+package com.example.ti.ble.sensortag;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.widget.Button;
+
+import com.example.ti.ble.sensortag.R;
+
+public class LicenseDialog extends Dialog {
+	private static LicenseDialog mDialog;
+	private static OkListener mOkListener;
+
+	public LicenseDialog(Context context) {
+		super(context);
+		mDialog = this;
+		mOkListener = new OkListener();
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.dialog_license);
+
+		// From license.html web page
+		WebView wv = (WebView) findViewById(R.id.webpage_license);
+		wv.loadUrl("file:///android_asset/license.html");
+
+		// Dismiss button
+		Button okButton = (Button) findViewById(R.id.buttonOK);
+		okButton.setOnClickListener(mOkListener);
+	}
+
+	private class OkListener implements android.view.View.OnClickListener {
+		@Override
+		public void onClick(View v) {
+			mDialog.dismiss();
+		}
+	}
+}
